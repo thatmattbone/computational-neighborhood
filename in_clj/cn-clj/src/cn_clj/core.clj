cbf2e72027b93246d251f266efbd0a5bebc85c34
Matt Bone <thatmattbone@gmail.com>
2010-11-18 21:44:36 -0600

    adding clojure skeleton project from leiningen

---
+++ in_clj/cn-clj/src/cn_clj/core.clj
@@ -0,0 +1,1 @@
+(ns cn-clj.core)
---
+++ in_clj/cn-clj/test/cn_clj/test/core.clj
@@ -0,0 +1,6 @@
+(ns cn-clj.test.core
+  (:use [cn-clj.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
